333c01300c9afb2a772cc469f2efac4db90394ae
Jonathan Hartman <j@p4nt5.com>
2016-09-18 13:59:04 -0700

    Initial commit; cookbook scaffolding

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+name 'openvpn_okta'
+maintainer 'Jonathan Hartman'
+maintainer_email 'jonathan.hartman@socrata.com'
+license 'apachev2'
+description 'Installs/configures the OpenVPN Okta plugin'
+long_description 'Installs/configures the OpenVPN Okta plugin'
+version '0.0.1'
+
+source_url 'https://github.com/socrata-cookbooks/openvpn_okta'
+issues_url 'https://github.com/socrata-cookbooks/openvpn_okta/issues'
+
+supports 'ubuntu'
